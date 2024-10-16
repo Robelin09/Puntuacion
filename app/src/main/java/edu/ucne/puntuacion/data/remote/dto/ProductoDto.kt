33b78a363b064b359ca68953a78817a1defd94cd
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-10-16 15:11:19 -0400

    ProductoDto creado

---
+++ app/src/main/java/edu/ucne/puntuacion/data/remote/dto/ProductoDto.kt
@@ -0,0 +1,8 @@
+package edu.ucne.puntuacion.data.remote.dto
+
+data class ProductoDto (
+    val productoId: Int,
+    val nombreProducto: String,
+    val descripcion: String,
+    val precio: Double,
+)
